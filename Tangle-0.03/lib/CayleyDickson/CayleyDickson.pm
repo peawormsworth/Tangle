a8445a8f57cbc3b03a67dab722ffdc12a62355c1
Jeff Anderson <peawormsworth@gmail.com>
2019-10-06 03:49:50 -0700

    Update CayleyDickson.pm

--- Tangle-0.03/lib/CayleyDickson/CayleyDickson.pm
+++ Tangle-0.03/lib/CayleyDickson/CayleyDickson.pm
@@ -9,10 +9,11 @@
 
 package CayleyDickson;
 use strict;
-use 5.010;
 no  warnings;
 use overload qw(- subtract + add * multiply / divide "" as_string eq eq);
 use constant SYMBOLS => ['', 'i' .. 'z', map('a' . $_, ('a' .. 'z')), ( map('b' . $_, ('a' .. 'z')) ) x 100];
+our $VERSION = 0.03;
+
 
 use constant DOUBLING_PRODUCT => 'Pt0';
 #
@@ -42,7 +43,7 @@ use constant I_SQUARED => -1;
 
 
 #
-# Conjugate: z* = (a,b)* = (-a,b*)
+# Conjugate: z* = (a,b)* = (a*,-b)
 #
 sub conjugate {
    my $m = shift;
@@ -152,16 +153,17 @@ sub multiply {
       $cs = $o;
       $ds =  0;
    }
-   given (DOUBLING_PRODUCT) {
-      (ref $m)->new($c * $a + $ii * $bs *  $d,  $d * $as +  $b *  $c) when  'P0';
-      (ref $m)->new($c * $a + $ii *  $d * $bs, $as *  $d +  $c *  $b) when  'P1';
-      (ref $m)->new($a * $c + $ii * $bs *  $d,  $d * $as +  $b *  $c) when  'P2';
-      (ref $m)->new($a * $c + $ii *  $d * $bs, $as *  $d +  $c *  $b) when  'P3';
-      (ref $m)->new($c * $a + $ii *  $b * $ds,  $a *  $d + $cs *  $b) when 'Pt0';
-      (ref $m)->new($c * $a + $ii * $ds *  $b,  $d *  $a +  $b * $cs) when 'Pt1';
-      (ref $m)->new($c * $a + $ii *  $b * $ds,  $a *  $d + $cs *  $b) when 'Pt2';
-      (ref $m)->new($a * $c + $ii * $ds *  $b,  $d *  $a +  $b * $cs) when 'Pt3';
-   }
+
+   # the eight ways to multiply Cayley Dickson number constructions...
+   my $dp = $m->doubling_product;
+   if    ($dp eq 'P0' ) { (ref $m)->new($c * $a + $ii * $bs *  $d,  $d * $as +  $b *  $c) }
+   elsif ($dp eq 'P1' ) { (ref $m)->new($c * $a + $ii *  $d * $bs, $as *  $d +  $c *  $b) }
+   elsif ($dp eq 'P2' ) { (ref $m)->new($a * $c + $ii * $bs *  $d,  $d * $as +  $b *  $c) }
+   elsif ($dp eq 'P3' ) { (ref $m)->new($a * $c + $ii *  $d * $bs, $as *  $d +  $c *  $b) }
+   elsif ($dp eq 'Pt0') { (ref $m)->new($c * $a + $ii *  $b * $ds,  $a *  $d + $cs *  $b) } # <= default
+   elsif ($dp eq 'Pt1') { (ref $m)->new($c * $a + $ii * $ds *  $b,  $d *  $a +  $b * $cs) }
+   elsif ($dp eq 'Pt2') { (ref $m)->new($c * $a + $ii *  $b * $ds,  $a *  $d + $cs *  $b) }
+   elsif ($dp eq 'Pt3') { (ref $m)->new($a * $c + $ii * $ds *  $b,  $d *  $a +  $b * $cs) }
 }
 
 
@@ -238,9 +240,16 @@ sub eq { shift->as_string eq shift }
 
 
 # 
-# Gamma: algebra selection: -1=cayley-dickson, 0=dual complex, 1=split complex
+# algebra selection. See I_SQUARED constant above for option choices. Override this method in your subclass if you like.
 #
 sub i_squared { I_SQUARED }
+
+
+
+# 
+# product product. See DOUBLING constant above for option choices. Override this method in your subclass if you like.
+#
+sub doubling_product { DOUBLING_PRODUCT }
 
 
 
@@ -267,45 +276,282 @@ sub _child_is {
    ref $a and $a->can($f) and $a->$f
 }
 
+=encoding utf8
+
+=pod
+
+=head1 NAME
+
+CayleyDickson - create and operate with hypercomplex numbers
+
+=head1 SYNOPSIS
+
+=over 4
+
+ use Tangle;
+ my $q1 = Tangle->new(1,0);
+ print "q1 = $q1\n";
+ $q1->x_gate;
+ print "X(q1) = $q1\n";
+ $q1->hadamard;
+ print "H(X(q1)) = $q1\n";
+
+ my $q2 = Tangle->new(1,0);
+ print "q2 = $q2\n";
+
+ # perform CNOT($q1 ⊗ $q2)
+ $q1->cnot($q2);
+
+ print "q1 = $q1\n";
+ print "q2 = $q2\n";
+
+ $q1->x_gate;
+ print "X(q1) = $q1\n";
+ print "entanglement causes q2 to automatically changed: $q2\n";
+
+=back
+
+=head1 DESCRIPTION
+
+=over 3
+
+ Cayley-Dickson construction and operations are defined here: https://en.wikipedia.org/wiki/Cayley–Dickson_construction
+
+ This object provides natural and intuitive operations on these numbers by overriding the native numeric operations (+,-,/,*)
+
+=back
+
+=head1 USAGE
 
 
-################################################
-# ... Cayley-Dickson algebriac functions ...
-#
-# Generate a new 
-#
-# Conjugate: z* = (a,b)* = (-a,b*)
-#
-# Invert: 1/z = z⁻¹ = (a,b)⁻¹ = (a,b)*/(norm(a,b)²)
-#
-# Norm: z->norm = √(norm(a)²+norm(b)²) 
-#
-# norm(number) = number
-#
-# Addition: z1+z2 = (a,b)+(c,d) = (a+c, b+d)
-#
-# Subtraction: (a,b)-(c,d) = (a-c, b-d)
-# $s: swap flag. Tells us that the calculation was like "2-(0,1)" but we received it in reverse order.
-#
-# Divide: z1/z2 = (a,b) × (c,d)⁻¹ = (a,b) × inverse(c,d)
-#   ... invert the divisor and then use multiplication instead.
-# Divide: (a,b)/n = (a,b) × 1/n
-#   ... if the divisor is just a number, invert it and use multiplication instead.
-# $s: swap flag. Tells us that the calculation was like "2/(0,1)" but we received it in reverse order.
-#
-# Multiply: (a,b)×(c,d) = (ac-d*b,da+bc*)
-# ... where z* represents the conjugate(z) where z is not a number.
-# ... where n* = n when n is a number.
-# $s: swap flag. Tells us that the calculation was like "2*(0,1)" but we received it in reverse order.
-#
-# Tensor: A⊗ B = (a,b)⊗ (c,d) => A = (ac, db, da, bc), B = (ac,bc,db,da)
-# given two object, move them into seperate spaces and tensor them,
-# so that their partial products are shared in memory...
-# Object construction, manipulation and debugging tools ...
-#
-# Standard POD documentation coming soon.
+=head2 new()
+
+=over 3
+
+ # create a new CayleyDickson number "i" ...
+ my $q1 = CayleyDickson->new(0,1);
+
+
+ # create a new CayleyDickson number "1+2i+3j+4k" ...
+ my $q2 = CayleyDickson->new(1,2,3,4);
+
+
+ # create a bigger CayleyDickson number (a Sedenion) ...
+ my $q3 = CayleyDickson->new(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+
+
+ # create a CayleyDickson number from others ...
+ my $one  = CayleyDickson->new(0,1);
+ my $zero = CayleyDickson->new(1,0);
+ my $quaternion = CayleyDickson->new($one,$zero);
+
+=back
+
+=head2 conjugate()
+
+=over 3
+
+   if z = (a,b)
+ then conjugate z = z* = (a,b)* = (a*,-b)
+   or conjugate(number) = number
+ 
+ printf "The conjugate of q1 is: %s\n", $q1->conjugate;
+
+=back
+
+=head2 inverse()
+
+=over 3
+
+   if z = (a,b)
+ then inverse z = z⁻¹ = (a,b)⁻¹ = (a,b)*/(norm(a,b)²)
+   or inverse(number) = number
+ 
+ printf "The inverse of q1 is: %s\n", $q1->inverse;
+
+=back
+
+=head2 norm()
+
+=over 3
+
+   if z = (a,b)
+ then norm z = norm(a,b) = √(norm(a)²+norm(b)²)
+   or norm(number) = number
+ 
+ printf "The norm of q1 is: %s\n", $q1->norm;
+
+=back
+
+=head2 add()
+
+=over 3
+
+ # ass z1 + z2 = (a,b)+(c,d) = (a+c,b+d)
+ 
+ printf "The sum of q1 + q2 is: %s\n", $q1 + $q2;
+
+=back
+
+=head2 subtract()
+
+=over 3
+
+ # subtract z1 - z2 =  (a,b)-(c,d) = (a-c,b-d)
+
+ printf "The difference of q1 - q2 is: %s\n", $q1 - $q2;
+
+=back
+
+=head2 divide()
+
+=over 3
+
+ # divide z1 / z2 = z1 × inverse(z2)
+ 
+ printf "The division of q1 / q2 is: %s\n", $q1 / $q2;
+
+=back
+
+=head2 multiply()
+
+=over 3
+
+ # Multiply: (a,b)×(c,d) = (a×c - d*×b, d×a + b×c*) where x* = conjugate(x) or x if x is a number
+
+ printf "The product of q1 * q2 is: %s\n", $q1 * $q2;
+
+=back
+
+=head2 new()
+
+=over 3
+
+  create a new CayleyDickson number of any size ...
+
+  # create the number 1+j-k ...
+  my $c = CayleyDickson->new( -1, 0, 1, -1 );
+
+  # create an octonion ...
+  my $c = CayleyDickson->new( 3, 7, -2, 8, 0, 3, 3, 5 );
+
+  # create a representation of the Horne bell state |+-> ...
+  my $c = CayleyDickson->new( 1/2, 1/2, 1/2 ,-1/2 );
+
+  # create a 128 number construction: 1+2i+3j+4k+ .. + 128 ....
+  my $c = CayleyDickson->new(1 .. 128);
+
+=back
+
+=head2 tensor()
+
+=over 3
+
+ Tensors two Cayley Dickson numbers to calculate a new number of higher dimensional construction.
+ reference: https://en.wikipedia.org/wiki/Tensor_product
+
+ # calculate the tensor of c1 ⊗  c2 ...
+ $d = $c1->tensor($c2);
+
+ $d will be a number of the product of the dimensions of c1 and c2.
+
+=back
+
+=head2 a()
+
+=head2 b()
+
+=over 3
+
+ returns the two objects or numbers held by this object
+
+=back
+
+=head2 flat()
+
+=over 3
+
+ return all the coefficients of the number as an array
+ 
+ printf "[%s]\n", join( ', ', $q1->flat); 
+
+
+=back
+
+=head2 as_string()
+
+=over 3
+
+ called automatically when this object is requested in a string form.
+ if you want to force the object to be resolved as a string ...
+
+ printf "q1 as a string = %s\n", $q1->as_string;
+
+=back
+
+=head2 i_squared()
+
+=over 3
+
+   returns the square of i: i² = -1
+
+   normally this will be -1, but you can change it to +1 or 0 using the constant I_SQUARED
+
+
+
+=back
+
+=head2 doubling_product()
+
+=over 3
+
+ something
+
+=back
+
+=head2 is_complex()
+
+=head2 is_quaternion()
+
+=head2 is_octonion()
+
+=head2 is_sedenion()
+
+=head2 is_trigintaduonions()
+
+=head2 is_sexagintaquatronions()
+
+=head2 is_centumduodetrigintanions()
+
+=head2 is_ducentiquinquagintasexions()
+
+ returns true if the given object has depth equal to the function name
+
+ if ($q1->is_octionion) {
+    print "q1 is an Octonion\n";
+ }
+ else {
+    print "q1 is NOT an Octonion\n";
+ }
+
+=back
+
+=head1 SUMMARY
+
+=over 3
+
+ This object holds Cayley Dickson numbers and provides math operations on them.
+
+ =back
+
+=head1 AUTHOR
+
+ Jeff Anderson
+ truejeffanderson@gmail.com
+
+=cut
+
 
 1;
 
 __END__
-
