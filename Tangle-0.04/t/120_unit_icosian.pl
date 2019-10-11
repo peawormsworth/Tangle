fa9c7336ab9ad36915a6b9050f66d1b0ce02b589
Jeff Anderson <peawormsworth@gmail.com>
2019-10-10 17:12:38 -0700

    refactor and simplify

--- Tangle-0.04/t/120_unit_icosian.pl
+++ Tangle-0.04/t/120_unit_icosian.pl
@@ -16,8 +16,10 @@ use lib qw(.);
 use CayleyDickson;
 use Data::Dumper;
 use utf8;
-use constant DEBUG   => 0;
-use constant VERBOSE => 1;
+use constant DEBUG     => 0;
+use constant VERBOSE   => 1;
+use constant PRECISION => 0.000000001;
+
 
 
 # golden ratio (φ) and golden conjugate (ϕ)
@@ -26,28 +28,28 @@ use constant VERBOSE => 1;
 #   φ =  –ϕ ⁻¹  and  ϕ =  –φ ⁻¹
 #
 
-use constant       POINT => 0;                                # zero dimension
-use constant        LINE => ( POINT  **   POINT );            #    1 dimension
-use constant          UP => +LINE;
-use constant        DOWN => -LINE;
-use constant        SPIN => ( UP      ,   DOWN  );            # polarity (+/-)
-use constant        AREA => ( LINE    +   LINE  );            #    2 dimension
-use constant   SPACETIME => ( AREA   **   AREA  );            #    4 dimension
-use constant IMAGINATION => ( AREA   **  -LINE  );            #  1/2 dimension. The place rotated half way between +1 and -1, a distance 1 from 0.
-use constant           φ => ((SPACETIME + LINE  ) ** IMAGINATION + LINE) * IMAGINATION; # golden ratio
-use constant           ϕ => ( -LINE   /   φ     );            # other side of the golden ratio (conjugate)
-use constant     ELEMENT => ( POINT   ,   LINE  ,  φ , ϕ );   # combination of 4 rotated elements
-use constant    ROTATION => Space->new(   POINT, POINT, SPIN  ) * IMAGINATION;
-use constant  INVOLUTION => 1 / ROTATION;
+use constant           POINT =>           0;                                     # zero dimension
+use constant            LINE => (     POINT **   POINT          );               #    1 dimension. Depends on CPU philosophy. What is 0⁰? https://youtu.be/r0_mi8ngNnM?t=750
+use constant           PLANE => (      LINE  +   LINE           );               #    2 dimension
+use constant       SPACETIME => (     PLANE **  PLANE           );               #    4 dimension
+use constant     IMAGINATION => (     PLANE **  -LINE           );               #  1/2 dimension. Half rotation between +1 and -1, a distance 1 from 0.
+use constant               φ => ((SPACETIME  +   LINE           ) ** IMAGINATION + LINE) * IMAGINATION; # golden ratio
+use constant               ϕ => (     -LINE  /   φ              );               # other side of the golden ratio (conjugate)
+use constant         SPIN_UP => (      LINE                     );               # spin up
+use constant       SPIN_DOWN => (  -SPIN_UP                     );               # spin down
+use constant            SPIN => (   SPIN_UP  ,   SPIN_DOWN      );               # polarity (+/-)
+use constant       AMPLITUDE => (     POINT  ,   SPIN_UP,  φ , ϕ);               # 4 probability amplitudes to be arranged
+use constant        ROTATION => Space->new( POINT, SPIN, POINT  ) * IMAGINATION; # 4d vector representing the rotational axis
+use constant      INVOLUTION => SPIN_UP / ROTATION;                              # the inverse of rotation required for completion
 
 
-my @ICOSIANS;
+my @icosians;
 
 foreach my $dimension (LINE .. SPACETIME) {
    foreach my $polarity (SPIN) {
-      my @dimensions = (POINT) x SPACETIME;
-      $dimensions[$dimension - LINE] = AREA * $polarity;
-      push @ICOSIANS, Space->new(@dimensions) * IMAGINATION;
+      my @vector = (POINT) x SPACETIME;
+      $vector[$dimension - LINE] =  PLANE * $polarity;
+      push @icosians, Space->new(@vector) * IMAGINATION;
    }
 }
 
@@ -56,32 +58,27 @@ foreach my $p1 (SPIN) {
    foreach my $p2 (SPIN) {
       foreach my $p3 (SPIN) {
          foreach my $p4 (SPIN) {
-            push @ICOSIANS, Space->new( $p1, $p2, $p3, $p4 ) * IMAGINATION;
+            push @icosians, Space->new( $p1, $p2, $p3, $p4 ) * IMAGINATION;
          }
       }
    }
 }
 
 
-foreach my $v1 (ELEMENT) {
-   my %set;
+foreach my $v1 (AMPLITUDE) {
    foreach my $p1 (SPIN) {
-      $set{$v1} = $p1;
-      foreach my $v2 (ELEMENT) {
-         next if $v2 == $v1;
+      foreach my $v2 (AMPLITUDE) {
          foreach my $p2 (SPIN) {
-            $set{$v2} = $p2;
-            foreach my $v3 (ELEMENT) {
-               next if ($v3 == $v2 or $v3 == $v1);
+            foreach my $v3 (AMPLITUDE) {
                foreach my $p3 (SPIN) {
-                  $set{$v3} = $p3;
-                  foreach my $v4 (ELEMENT) {
-                     next if ($v4 == $v3 or $v4 == $v2 or $v4 == $v1);
+                  foreach my $v4 (AMPLITUDE) {
                      foreach my $p4 (SPIN) {
-                        $set{$v4} = $p4;
-                        next if $set{+POINT} == -LINE;
-                        next if $set{+φ } == $set{+ϕ};
-                        push @ICOSIANS, Space->new( $p1*$v1, $p2*$v2, $p3*$v3, $p4*$v4 ) * IMAGINATION;
+                        my %vector;
+                        @vector{ $v1, $v2, $v3, $v4 } = ( $p1, $p2, $p3, $p4 );
+                        next if keys %vector         != SPACETIME
+                             or      $vector{+POINT} == -LINE
+                             or      $vector{  +φ  } == $vector{ +ϕ };
+                        push @icosians, Space->new( $v1*$p1, $v2*$p2, $v3*$p3, $v4*$p4 ) * IMAGINATION;
                      }
                   }
                }
@@ -93,38 +90,52 @@ foreach my $v1 (ELEMENT) {
 
 #
 # Rotate the object by some amount
+#
+
+printf "\nRotate 600-cell by ROTATION: %s\n\n", ROTATION if VERBOSE;
 
 my @rotated;
-foreach my $v (@ICOSIANS) {
-   push @rotated, ROTATION * $v * INVOLUTION;
+foreach my $vector (@icosians) {
+   push @rotated, ROTATION * $vector * INVOLUTION;
 }
 
 
 #
-# Output results ...
+#  Output ...
 #
 
-print "120 Quaterions representing the vertex points of the 600-cell:\n\n";
+print "\n\nOriginal 120 Quaterions representing the vertex points of the 600-cell:\n\n";
 
 if (VERBOSE) {
    my $i = 1;
-   foreach my $v (@ICOSIANS) {
-      printf "vertex %5d: [%s]\n", $i++, join(', ', map(sprintf('  %s%-s', ($_ < 0 ? '-' : '+' ), sprintf('%0.5f', abs($_))), $v->flat));
+   foreach my $vector (@icosians) {
+      printf "vertex %5d: [%s]\n", $i++, basis($vector);
    }
 }
 
+printf "\n\n120 Quaterions vertex points rotated by %s\n\n", ROTATION;
 
-print "\nRotated 600-cell by ROTATION:\n\n";
 
 if (VERBOSE) {
    my $i = 1;
-   foreach my $v (@rotated) {
-      printf "vertex %5d: [%s]\n", $i++, join(', ', map(sprintf('  %s%-s', ($_ < 0 ? '-' : '+' ), sprintf('%0.5f', abs($_))), $v->flat));
+   foreach my $vector (@rotated) {
+      printf "vertex %5d: [%s]\n", $i++, basis($vector);
    }
 }
 
-d(icosians => \@ICOSIANS) if DEBUG;
-printf "Total elements: %d\n", scalar @ICOSIANS;
+d(icosians => \@icosians) if DEBUG;
+
+printf "Total elements: %d\n", scalar @icosians;
+
+
+# 
+# function tools ...
+#
+
+sub basis {
+   my $vector = shift;
+   join(', ', map(sprintf('  %s%-s', ($_ < 0 ? '-' : '+' ), sprintf('%0.5f', abs($_))), $vector->flat))
+}
 
 sub d {
    my %a = @_;
